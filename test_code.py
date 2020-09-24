554fa1d9624347874950a27d65cfaac7f3b1374b
fonzy <fonzynice@gmail.com>
2020-09-24 15:09:15 -0400

    Files updated

--- test_code.py
+++ test_code.py
@@ -1,11 +1,11 @@
-from netdemo.mynornir import title, print_title
-
-c = title('Connecting to Cisco Dev-net Sandbox...')
-print_title(c)
+# from netdemo.mynornir import title, print_title
+#
+# c = title('Connecting to Cisco Dev-net Sandbox...')
+# print_title(c)
 
 
 def test_title():
-    # name = 'cisco'
+    c = 'Connecting to Cisco Dev-net Sandbox...'
     assert c == 'Connecting to Cisco Dev-net Sandbox...'
 
 
