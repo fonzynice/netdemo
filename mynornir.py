2fdf51913471ba9899708ac6709cf68581da474e
fonzy <fonzynice@gmail.com>
2020-09-24 14:18:09 -0400

    Files updated

--- mynornir.py
+++ mynornir.py
@@ -1,21 +1,27 @@
 from nornir import InitNornir
+from nornir_utils.plugins.functions import print_result, print_title
 from nornir_napalm.plugins.tasks import napalm_get
 from nornir_netmiko.tasks.netmiko_send_command import netmiko_send_command
 import pprint
+import ipdb
+import json
 
 name = 'cisco'
 
 nr = InitNornir(config_file="config.yaml")
-r2 = nr.run(
-    task=netmiko_send_command,
-    command_string="show ip route"
-)
+get_info = nr.run(
+           task=netmiko_send_command,
+           command_string="show ip route"
+            )
 
-r = nr.run(
-    task=napalm_get,
-    getters=["facts"]
-)
-pprint.pprint(r["rtr1"][0].result)
+get_info_napalm = nr.run(
+        task=napalm_get,
+        getters=["facts"]
+    )
+
+# ipdb.set_trace()
+print_result(get_info)
+# pprint.pprint(get_info_napalm["rtr1"][0].result)
 
 
 
